c241ba53a3241387a678b1412d98dfdf3c37c1bd
Myles Lorenz Waite <awsome2134@Myless-MacBook-Pro.local>
2020-02-12 11:43:41 -0500

    Updates

---
+++ precache-manifest.5f34e91c4bf43b10b0f859f5b9377889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760bc6eecabf38ba08a7435b6c53598e",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "b06cc5e4d050b3e5496f",
+    "url": "/dig4639-mobile-dev/static/css/main.4afa2deb.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06cc5e4d050b3e5496f",
+    "url": "/dig4639-mobile-dev/static/js/main.f3064b0f.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
