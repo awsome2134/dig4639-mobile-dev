e53d909cc6bef4f0570d20ef7295772565d7eb94
Myles Lorenz Waite <awsome2134@Myless-MacBook-Pro.local>
2020-03-01 18:05:19 -0500

    will not remove

---
+++ labs/4/src/components/Card/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+class Card extends React.Component{
+    render (){
+        return(
+            <div className="card">
+                <h3>{this.props.title}</h3>
+                <p>{this.props.content}</p>
+                <button className="close" value="close" onClick={() => this.props.removeCard(this.props.id)}></button>
+            </div>
+        );
+    }
+}
+
+export default Card;
---
+++ labs/4/src/components/CardList/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "../Card/index.js";
+import cardList from "../../data.json";
+
+class CardList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            cardList
+        }
+        this.currentId=0;
+        this.card_List=this.addIds();
+      }
+
+      addIds(){
+          let card_List= [];
+          this.state.cardList.cards.forEach(value => {
+              card_List.push({"id": this.currentId, "title": value.title, "content": value.content});
+              this.currentId++;
+          });
+          console.log(card_List);
+          return card_List;
+      }
+
+      removeCard(id){
+        console.log(this.card_List);
+        let cardList=this.card_List;
+        cardList=cardList.filter((v) => v.id !== id);
+        console.log(this.card_List);
+        this.card_List=cardList;
+    
+      }
+      render(){
+        return(
+            <React.Fragment>
+                {this.card_List.map((v) => 
+                    <Card id={v.id}  title={v.title} removeCard={(id) => this.removeCard(id)} content={v.content}  key={v.id} />)}
+            </React.Fragment>
+        );
+      }
+}
+
+export default CardList;
